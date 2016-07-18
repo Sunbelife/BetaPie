bba1610288e6bd08b75f1d1083866702b7099965
Jacky <jacky-943572677@qq.com>
2016-07-18 22:45:52 +0800

    仅供测试

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--link favicon.-->
+    <title>尝鲜派 | BetaPie</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet"  href="css/betapie.css">
+    <link rel="stylesheet"  href="css/bootstrap.css">
+    <link href="http://cdn.bootcss.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <!--导航栏-->
+<nav class="navbar navbar-default" role="navigation" style="margin-bottom:0px;">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+        <a class="navbar-brand" href="#">尝鲜派</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="navbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="index.html">首页</a></li>
+        <li><a href="sn.html">序列号查询</a></li>
+        <li><a href="download.html">固件下载</a></li>
+        <li><a href="#">前往论坛</a></li>
+        <li><a href="http://weibo.com/xxxxxx">关注微博</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<!--巨幕-->
+<div class="main" style="margin-top:60px;" align="center">
+  <div class="container" style="margin-bottom: 60px;margin: 0 auto;max-width: 80%;" align="center">
+    <div class="img">
+      <img src="imgs/5.jpg" width="90%">
+    </div>
+    <h1 class="Welcome" style="font-size:60px;margin-top:30px;">欢迎来到尝鲜派<img src="imgs/logo.png" style="vertical-align:top" width="20"></h1>
+    <h2 class="introduce" style="font-size:24px;margin-top:30px;margin-bottom:30px;">在这里，您既可以帮助苹果把即将发布的 iOS 10 和 watchOS 3 做成史上最佳的版本，亦可以查看新系统的体验报告，第一时间了解新系统改进之处与体验效果。</h2>
+    <hr>
+  </div>
+<!--内容-->
+  <div class="content" style="margin-bottom:30px;argin: 0 auto;max-width: 80%">
+    <div class="ios" style="max-width: 83.3%">
+    <h3 class="ios10" style="font-size:42px;margin-bottom: 30px;">开始使用 iOS 10</h3>
+    <h4 style="text-align: left;">1.归档 iOS 备份</h4>
+    <p style="text-align: left;">请注意，您即将安装 Beta 版软件。请始终在安装 Beta 版 iOS 前备份您的数据和文件。我们建议您在安装 iOS Beta 版前进行 iTunes 备份并将其归档。</p>
+    <p style="text-align: left;margin-bottom:30px;"><a href="https://support.apple.com/HT203282">使用 iTunes 归档 iOS 备份 &gt;</a></p>
+    <img src="imgs/6.jpg" width="90%">
+    <hr>
+    <h4 style="text-align: left;">2.下载描述文件</h4>
+    <p style="text-align: left;">通过 iOS 设备点击下方的下载按钮，以下载配置描述文件。您的 iOS 设备将会提示您进行安装。按照屏幕指示来允许安装。</p>
+    <div class="button" style="text-align: left;margin-bottom: 30px">
+    <button type="button" class="btn btn-info"><a href="profiles/iOS_10_beta_Configuration_Profile.mobileconfig" style="color:#fff; text-decoration:none;">iOS 设备下载</a></button>
+    </div>
+    <img src="imgs/3.jpg" width=90%>
+    <hr>
+    <h4 style="text-align: left;">3.更新您的 iOS 设备</h4>
+    <p style="text-align: left;">运行 iOS 8.1.3 或更高版本的 iOS 设备将以无线方式自动接收最新的 Public Beta 版软件。轻点“设置”>“通用”>“软件更新”来安装任何可用的 Beta 版软件。</p>
+    <img src="imgs/4.jpg" width=90%>
+    <hr>
+  </div>
+</div>
+</div>
+<!---->
+<!--footer...-->
+<div class="footer" style="color:#5e5e5e;text-align:center;padding-top:20px;padding-bottom:20px;padding-right:0px;padding-left:0px;font-size:13px;line-height:25px;">
+      <span>Copyright © 尝鲜派 2016</span><br>
+      <span>京ICP备案20162333号</span><br>
+	</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="http://cdn.bootcss.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  </body>
+</html>
